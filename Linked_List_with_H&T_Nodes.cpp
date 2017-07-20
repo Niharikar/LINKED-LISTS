a67b521688e960b2e4bcbbd14715c849332eaadb
Niharikar Reddy <niharikar005@gmail.com>
2017-07-20 11:12:10 +0530

    Create Linked_List_with_H&T_Nodes.cpp

---
+++ Linked_List_with_H&T_Nodes.cpp
@@ -0,0 +1,220 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+struct Node
+{
+  int data;
+  Node* next;
+};
+
+class Xsll
+{
+   Node* head;
+   Node* tail; 
+   public:
+    Xsll()
+    {
+      Node* temp = new Node;
+      Node* temp1 = new Node;
+      temp->next = temp1;
+      temp1->next = NULL;
+      
+      head = temp;
+      tail = temp1;
+    }
+    
+    ~Xsll()
+    {
+       DeleteList();   
+    }
+    
+    int Nodes()  //Number of Nodes
+    {
+        if(head->next != tail)
+        {
+            Node* t = head->next;
+            int k = 1;
+            while(t->next != tail)
+            {
+                t = t->next;
+                k++;
+            }
+
+            return k;
+        }
+
+        else
+        {
+            return 0;
+        }
+      
+    }
+  
+  
+    int NodeValue(int n)  // Value of a particular Node
+    {
+           
+        try
+        {
+          if(head->next!=tail)
+          {
+               Node* t = head->next;
+                if(n>0 && n<= Nodes())
+                {
+
+                    for(int i = 2;i <= n;i++)
+                    {
+                        t = t->next;
+                    }
+                }
+
+               else
+               {
+                  throw 0;
+               }
+           
+           return t->data;
+          }
+        }
+      
+         catch(int e)
+         {
+           cout <<"Invalid position number entered"<<endl; 
+         }
+        
+    }
+    void InsertHead(int x)
+    {
+        Node* t = new Node;
+        t->next = head->next;
+        t->data = x;
+        head->next = t;                    
+    }
+    void AppendNode(int x)
+    {
+        if(head->next == tail)
+        {
+          InsertHead(x);
+        }
+        else
+        {
+            Node* te = head->next;
+          
+            while(te->next != tail)
+            {
+                te = te->next;
+            }
+            Node* t = new Node;
+            t->next = tail;
+            t->data = x;
+            te->next = t;
+        }
+      
+    }
+
+    void RemoveNode(int n)  // 'n' is position of node
+    {
+      
+        if(n<=Nodes() && n>0)
+        {
+
+                if(n==1)  
+                {                                         
+                    Node* temp = head->next;
+                    head->next = temp->next;
+                    free(temp);
+                }
+
+                else
+                { 
+
+                      Node* t = head->next;
+                      Node* te = head->next;
+                      for(int i = 2;i<=(n);i++)
+                      {
+                        t = t->next;
+                        if(i<=(n-1))
+                          te = te->next;
+                      }
+
+                      if(t->next==tail)
+                      {
+
+                          Node* temp = t;
+                          te->next = tail;
+                          free(temp);                                                                
+                      }
+
+
+                      else
+                      {                    
+                          te->next = t->next;
+                          free(t);                 
+                      }
+
+                }
+        }
+
+        else
+        {
+           cout << "Invalid position number is entered" << endl; 
+        }
+             
+    }
+    
+    void DeleteList()
+    {
+        Node* temp = head->next;
+        Node* t;
+        while(temp!=tail)
+        {
+            t = temp->next;
+            free(temp);
+            temp = t;
+        }
+        
+        head->next = tail;
+    }
+
+ 
+    
+};
+
+int main()
+{
+    Xsll l1,l2;
+  for(int i = 0;i<1000;i++)
+  {
+    l1.InsertHead(rand()%100);
+  }
+  
+  for(int i = 0;i<1000;i++)
+  {
+      l1.AppendNode(rand()%100);
+  }
+  
+  cout <<l1.Nodes() << endl;
+  
+    for(int i = 0;i<1000;i++)
+    {
+      l1.RemoveNode(1);
+    } 
+  
+ cout << l1.Nodes() << endl;
+ 
+  
+   for(int i = 0;i<1000;i++)
+  {
+      l1.AppendNode(rand()%100);
+  }
+  cout << l1.Nodes() << endl;
+  
+  cout << l1.NodeValue(45) << endl;
+  
+  cout << l1.NodeValue(100) << endl;
+  
+  l1.DeleteList();
+  cout << l1.Nodes() << endl; 
+    
+}
